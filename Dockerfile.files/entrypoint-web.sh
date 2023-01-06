4ecbc0b0268c9d191ec0527f2f22eef28e5bd81f
luizeof <luizeof@gmail.com>
2023-01-06 01:22:55 -0800

    fix

--- Dockerfile.files/entrypoint-web.sh
+++ Dockerfile.files/entrypoint-web.sh
@@ -5,23 +5,22 @@ cd /var/www/html
 
 cp -f /var/www/html/.env.example /var/www/html/.env
 
-"DB_CONNECTION=$DB_CONNECTION" >> /var/www/html/.env
-"DB_HOST=$DB_HOST" >> /var/www/html/.env
-"DB_PORT=$DB_PORT" >> /var/www/html/.env
-"DB_DATABASE=$DB_DATABASE" >> /var/www/html/.env
-"DB_USERNAME=$DB_USERNAME" >> /var/www/html/.env
-"DB_PASSWORD=$DB_PASSWORD" >> /var/www/html/.env
+echo "DB_CONNECTION=$DB_CONNECTION" >> /var/www/html/.env
+echo "DB_HOST=$DB_HOST" >> /var/www/html/.env
+echo "DB_PORT=$DB_PORT" >> /var/www/html/.env
+echo "DB_DATABASE=$DB_DATABASE" >> /var/www/html/.env
+echo "DB_USERNAME=$DB_USERNAME" >> /var/www/html/.env
+echo "DB_PASSWORD=$DB_PASSWORD" >> /var/www/html/.env
 
-"REDIS_HOST=$REDIS_HOST" >> /var/www/html/.env
-"REDIS_PASSWORD=$REDIS_PASSWORD" >> /var/www/html/.env
-"REDIS_PORT=$REDIS_PORT" >> /var/www/html/.env
+echo "REDIS_HOST=$REDIS_HOST" >> /var/www/html/.env
+echo "REDIS_PASSWORD=$REDIS_PASSWORD" >> /var/www/html/.env
+echo "REDIS_PORT=$REDIS_PORT" >> /var/www/html/.env
 
-"SESSION_DRIVER=redis" >> /var/www/html/.env
-"CACHE_DRIVER=redis" >> /var/www/html/.env
+echo "SESSION_DRIVER=redis" >> /var/www/html/.env
+echo "CACHE_DRIVER=redis" >> /var/www/html/.env
 
-"APP_KEY=$APP_KEY" >> /var/www/html/.env
-"APP_URL=$APP_URL" >> /var/www/html/.env
-
+echo "APP_KEY=$APP_KEY" >> /var/www/html/.env
+echo "APP_URL=$APP_URL" >> /var/www/html/.env
 
 sudo -u www-data php /var/www/html/artisan config:clear
 
