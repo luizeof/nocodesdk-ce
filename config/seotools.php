b808ba50f3e7643ec591d96aa41d9ac395597a30
luizeof <luizeof@gmail.com>
2023-03-04 07:31:32 -0800

    packages

--- config/seotools.php
+++ config/seotools.php
@@ -9,7 +9,7 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'        => "NoCode SDK", // set false to total remove
+            'title'        => "NoCodeSDK", // set false to total remove
             'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
             'description'  => 'Take control of your App', // set false to total remove
             'separator'    => ' - ',
@@ -36,7 +36,7 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'NoCode SDK Panel', // set false to total remove
+            'title'       => 'NoCodeSDK', // set false to total remove
             'description' => 'Take control of your App', // set false to total remove
             'url'         => false, // Set null for using Url::current(), set false to total remove
             'type'        => false,
@@ -58,7 +58,7 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'NoCode SDK Panel', // set false to total remove
+            'title'       => 'NoCodeSDK', // set false to total remove
             'description' => 'Take control of your App', // set false to total remove
             'url'         => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
             'type'        => 'WebPage',
